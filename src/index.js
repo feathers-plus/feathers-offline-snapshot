d671192e06d710a4046fe3ad66a321ecb207489b
John Szwaronek <johnsz9999@gmail.com>
2017-06-08 09:51:30 -0400

    Initial commit of code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-// import errors from 'feathers-errors';
+
 import makeDebug from 'debug';
 
 const debug = makeDebug('feathers-offline-snapshot');
 
-export default function init () {
-  debug('Initializing feathers-offline-snapshot plugin');
-  return 'feathers-offline-snapshot';
+export default function snapshot(service, baseQuery) {
+  debug(`start: ${JSON.stringify(baseQuery)}`);
+  
+  const query = Object.assign({}, { $skip: 0, $limit: 200 }, baseQuery); // use max recs configured
+  let fileDatas;
+  
+  return service.find({ query })
+    .then(result => {
+      debug(`read ${(result.data || result).length} records`);
+      
+      if (!result.data) {
+        return result;
+      }
+    
+      const { total, limit, skip, data } = result;
+      fileDatas = data;
+
+      return (skip + data.length < total) ? readRemainingPages(skip + limit) : fileDatas;
+    });
+  
+  function readRemainingPages (skip) {
+    query.$skip = skip;
+    
+    return service.find({ query })
+      .then(({ total, limit, skip, data }) => {
+        debug(`read ${data.length} records`);
+        
+        fileDatas = fileDatas.concat(data);
+  
+        return (skip + data.length < total) ? readRemainingPages(skip + limit) : fileDatas;
+      });
+  }
 }
---
+++ test/snapshot.test.js
@@ -0,0 +1,119 @@
+
+const assert = require('chai').assert;
+const feathers = require('feathers');
+const memory = require('feathers-memory');
+const hooks = require('feathers-hooks');
+
+const snapshot = require('../src');
+
+const sampleLen = 25;
+let data;
+let dataConverted;
+let fromService;
+let fromServicePaginated;
+let toService;
+
+function services1 () {
+  const app = this;
+
+  app.configure(fromServiceNonPaginatedConfig);
+  app.configure(fromServicePaginatedConfig);
+}
+
+function fromServiceNonPaginatedConfig () {
+  const app = this;
+
+  app.use('/from', memory({}));
+}
+
+function fromServicePaginatedConfig () {
+  const app = this;
+
+  app.use('/frompaginated', memory({
+    paginate: {
+      default: 2,
+      max: 3
+    }
+  }));
+}
+
+function convert (rec) {
+  rec.id = rec.__id; // feathers-memory does not add ids to rec, so fake it
+  rec.uuid = rec.__id;
+  return rec;
+}
+
+describe('snapshotutils-copy:', () => {
+  beforeEach(() => {
+    const app = feathers()
+      .configure(hooks())
+      .configure(services1);
+
+    fromService = app.service('from');
+    fromServicePaginated = app.service('frompaginated');
+
+    data = [];
+    dataConverted = [];
+    for (let i = 0, len = sampleLen; i < len; i += 1) {
+      data.push({ id: i, order: i });
+      dataConverted.push({ id: i, order: i, uuid: i, __id: i });
+    }
+
+    return Promise.all([
+      fromService.create(data),
+      fromServicePaginated.create(data)
+    ]);
+  });
+
+  describe('read file', () => {
+    it('non-paginated file', () => {
+      return snapshot(fromService)
+        .then(result => {
+          assert.lengthOf(result, sampleLen);
+
+          assert.deepEqual(sortArrayByProp(result, 'order'), data);
+        })
+        .catch(logAndThrow('copy error'));
+    });
+
+    it('paginated file', () => {
+      return snapshot(fromServicePaginated)
+        .then(result => {
+          assert.lengthOf(result, sampleLen);
+
+          assert.deepEqual(sortArrayByProp(result, 'order'), data);
+        })
+        .catch(logAndThrow('copy error'));
+    });
+
+    it('selection works', () => {
+      const selection = { order: { $lt: 15 } };
+
+      return snapshot(fromServicePaginated, selection)
+        .then(result => {
+          assert.lengthOf(result, 15);
+
+          assert.deepEqual(sortArrayByProp(result, 'order'), data.slice(0, 15));
+        })
+        .catch(logAndThrow('copy error'));
+    });
+  });
+});
+
+// Helpers
+
+function sortArrayByProp (array, key) {
+  return array.sort((a, b) => {
+    const x = a[key];
+    const y = b[key];
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+}
+
+function logAndThrow(msg) {
+  return err => {
+    console.log(msg, err.message);
+    console.log(err.stack);
+    throw err;
+  };
+}
