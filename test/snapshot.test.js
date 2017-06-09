27f47fc5df56279866bcfcf8c5c3296bb6352189
John Szwaronek <johnsz9999@gmail.com>
2017-06-08 20:33:12 -0400

    Improved code quality test

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -41,55 +41,53 @@ describe('snapshotutils-copy:', () => {
     const app = feathers()
       .configure(hooks())
       .configure(services1);
-
+    
     fromService = app.service('from');
     fromServicePaginated = app.service('frompaginated');
-
+    
     data = [];
     dataConverted = [];
     for (let i = 0, len = sampleLen; i < len; i += 1) {
       data.push({ id: i, order: i });
       dataConverted.push({ id: i, order: i, uuid: i, __id: i });
     }
-
+    
     return Promise.all([
       fromService.create(data),
       fromServicePaginated.create(data)
     ]);
   });
-
-  describe('read file', () => {
-    it('non-paginated file', () => {
-      return snapshot(fromService)
-        .then(result => {
-          assert.lengthOf(result, sampleLen);
-
-          assert.deepEqual(sortArrayByProp(result, 'order'), data);
-        })
-        .catch(logAndThrow('copy error'));
-    });
-
-    it('paginated file', () => {
-      return snapshot(fromServicePaginated)
-        .then(result => {
-          assert.lengthOf(result, sampleLen);
-
-          assert.deepEqual(sortArrayByProp(result, 'order'), data);
-        })
-        .catch(logAndThrow('copy error'));
-    });
-
-    it('selection works', () => {
-      const selection = { order: { $lt: 15 } };
-
-      return snapshot(fromServicePaginated, selection)
-        .then(result => {
-          assert.lengthOf(result, 15);
-
-          assert.deepEqual(sortArrayByProp(result, 'order'), data.slice(0, 15));
-        })
-        .catch(logAndThrow('copy error'));
-    });
+  
+  it('non-paginated file', () => {
+    return snapshot(fromService)
+      .then(result => {
+        assert.lengthOf(result, sampleLen);
+        
+        assert.deepEqual(sortArrayByProp(result, 'order'), data);
+      })
+      .catch(logAndThrow('copy error'));
+  });
+  
+  it('paginated file', () => {
+    return snapshot(fromServicePaginated)
+      .then(result => {
+        assert.lengthOf(result, sampleLen);
+        
+        assert.deepEqual(sortArrayByProp(result, 'order'), data);
+      })
+      .catch(logAndThrow('copy error'));
+  });
+  
+  it('selection works', () => {
+    const selection = { order: { $lt: 15 } };
+    
+    return snapshot(fromServicePaginated, selection)
+      .then(result => {
+        assert.lengthOf(result, 15);
+        
+        assert.deepEqual(sortArrayByProp(result, 'order'), data.slice(0, 15));
+      })
+      .catch(logAndThrow('copy error'));
   });
 });
 
